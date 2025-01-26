8a2dd0263687f260efa7af7dcf859460a881b100
Ilia Vasilkevich <vimybox@gmail.com>
2025-01-26 20:31:45 +0300

    [feat] general component with dropdown

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import StatusDropdown from './components/StatusDropdown/StatusDropdown.vue';
+import StatusDropdown from './components/DropdownComp/DropdownComp.vue';
+import { ref } from 'vue';
+
+const list = [
+  { id: 'draft', label: 'draft', style: 'bg-white border-1' },
+  { id: 'completed', label: 'completed', style: 'bg-green-100 text-green-700' },
+  { id: 'failed', label: 'failed', style: 'bg-red-100 text-red-600' },
+];
+
+const status = ref<(typeof list)[0] | null>(list[0]);
 </script>
 
 <template>
-  <StatusDropdown />
+  <StatusDropdown :list="list" v-model="status" />
 </template>
-
-<style scoped>
-</style>
