8e843ba89aec749aa2ec3004eac140294789ca7a
Ilia Vasilkevich <vimybox@gmail.com>
2025-01-26 20:29:58 +0300

    [feat] add disabled option

--- src/components/DropdownComp/DropdownItem.vue
+++ src/components/DropdownComp/DropdownItem.vue
@@ -1,5 +1,9 @@
 <template>
-  <button class="py-1.5 px-4 hover:bg-gray-300 cursor-pointer w-full text-left flex text-xs">
+  <button
+    :disabled="item.disabled"
+    class="py-1.5 px-4 w-full text-left flex text-xs"
+    :class="{ 'cursor-pointer hover:bg-gray-300': !item.disabled }"
+  >
     <span class="px-2 rounded-xs uppercase h-5 flex items-center" :class="item.style">
       {{ item.label }}
     </span>
@@ -11,6 +15,7 @@ export type Item = {
   id: string;
   label: string;
   style?: string;
+  disabled?: boolean;
 };
 defineProps<{ item: Item }>();
 </script>
