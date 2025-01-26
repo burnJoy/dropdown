05202b36766f180a89177eb8be8687fb935db310
Ilia Vasilkevich <vimybox@gmail.com>
2025-01-26 12:44:31 +0300

    [init] start vite+ts+sb+tailwind

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import StatusDropdown from './components/StatusDropdown/StatusDropdown.vue';
+</script>
+
+<template>
+  <StatusDropdown />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/StatusDropdown/StatusDropdown.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>dropdown</div>
+</template>
+
+<style scoped></style>
