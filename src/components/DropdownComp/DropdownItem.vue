d03fed04b202b7fcc26b8f7b3f98c20e0ebc2592
Ilia Vasilkevich <vimybox@gmail.com>
2025-01-26 20:21:10 +0300

    [feat] dropdown item

---
+++ src/components/DropdownComp/DropdownItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="py-1.5 px-4 hover:bg-gray-300 cursor-pointer w-full text-left flex text-xs">
+    <span class="px-2 rounded-xs uppercase h-5 flex items-center" :class="item.style">
+      {{ item.label }}
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+export type Item = {
+  id: string;
+  label: string;
+  style?: string;
+};
+defineProps<{ item: Item }>();
+</script>
